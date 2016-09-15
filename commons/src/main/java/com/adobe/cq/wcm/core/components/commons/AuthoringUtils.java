98fc429d7e2ca3c079172d01b152db557ee3cc8e
Burkhard Pauli <pauli@adobe.com>
2016-09-15 15:38:33 +0200

    CQ-97655: [CoreComponents] Empty placeholder should display component's title
    Move getComponentTitle to more self-explanatory ComponentUtils class
    (RTC not needed)

--- commons/src/main/java/com/adobe/cq/wcm/core/components/commons/AuthoringUtils.java
+++ commons/src/main/java/com/adobe/cq/wcm/core/components/commons/AuthoringUtils.java
@@ -17,12 +17,7 @@ package com.adobe.cq.wcm.core.components.commons;
 
 import javax.servlet.ServletRequest;
 
-import org.apache.commons.lang.StringUtils;
-import org.apache.sling.api.resource.Resource;
-
 import com.day.cq.wcm.api.AuthoringUIMode;
-import com.day.cq.wcm.api.components.Component;
-import com.day.cq.wcm.commons.WCMUtils;
 
 public final class AuthoringUtils {
 
@@ -46,21 +41,5 @@ public final class AuthoringUtils {
     public static boolean isClassic(ServletRequest request) {
         AuthoringUIMode currentMode = AuthoringUIMode.fromRequest(request);
         return AuthoringUIMode.CLASSIC == currentMode;
-    }
-
-    /**
-     * Get the current component title form the resource
-     *
-     * @param resource the current resource
-     * @param defaultTitle the default if component is null or title is empty
-     * @return {@code String} of component tile or defaultTitle
-     */
-    public static String getComponentTitle(Resource resource, String defaultTitle) {
-        String title = StringUtils.defaultString(defaultTitle, StringUtils.EMPTY);
-        Component component = WCMUtils.getComponent(resource);
-        if(component != null) {
-            title = StringUtils.defaultIfEmpty(component.getTitle(), title) ;
-        }
-        return title;
     }
 }
