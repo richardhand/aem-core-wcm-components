5cacc6c4502792342894456f680aea9026110e7b
Levente Santha <sntha@adobe.com>
2016-09-21 20:26:29 +0300

    CQ-98834 - Implement sharing component in AEM Core WCM project
     * added new sharing component in page component project
     * added support for social media libraries to page component
     * restructured Java helper code related to social media sharing into SocialMediaHelper of page component, used also in sharing component

--- page/src/content/jcr_root/apps/core/wcm/components/page/v1/page/SocialMediaHelper.java
+++ page/src/content/jcr_root/apps/core/wcm/components/page/v1/page/SocialMediaHelper.java
@@ -42,25 +42,109 @@ import java.util.Map;
 import java.util.Set;
 
 /**
- * Generates Open Graph metadata for a page to be shared on social media services.
+ * Helper class for page functionality related to page sharing by user on social media platforms.
  */
-public class SocialMediaMetadata extends WCMUsePojo {
-    private static final Logger LOGGER = LoggerFactory.getLogger(SocialMediaMetadata.class);
+public class SocialMediaHelper extends WCMUsePojo {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SocialMediaHelper.class);
     private static final String EXPERIENCE_FRAGMENT_CONTAINER = "cq/experience-fragments/editor/components/experiencefragment";
     private static final String FACEBOOK_VARIATION = "cq/experience-fragments/components/xffacebook";
     private static final String PINTEREST_VARIATION = "cq/experience-fragments/components/xfpinterest";
+    private static final String SHARING_COMPONENT = "core/wcm/components/sharing";
+    private boolean facebookEnabled;
+    private boolean pinterestEnabled;
+    private boolean socialMediaEnabled;
+    /**
+     * Holds the metadata for a page.
+     */
+    private Map<String, String> metadata;
+    private boolean hasSharingComponent;
+
+    //*************** SIGHTLY METHODS *******************
+    /**
+     * Returns {@code true} if Facebook sharing is enabled in page configuration, {@code false} otherwise.
+     */
+    public boolean isFacebookEnabled() {
+        return facebookEnabled;
+    }
 
     /**
-     * Hold the metadata for a page.
+     * Returns {@code true} if Pinterest sharing is enabled in page configuration, {@code false} otherwise.
      */
-    private Map<String, String> metadata = new LinkedHashMap<>();
+    public boolean isPinterestEnabled() {
+        return facebookEnabled;
+    }
 
+    /**
+     * Returns true if a supported social media sharing is enabled in page configuration, false otherwise.
+     */
+    public boolean isSocialMediaEnabled() {
+        return socialMediaEnabled;
+    }
+
+    /**
+     * Returns {@code true} if Facebook sharing is enabled in page configuration
+     * and the page contains the sharing component, {@code false} otherwise.
+     */
+    public boolean hasFacebookSharing() {
+        return facebookEnabled && hasSharingComponent;
+    }
+
+    /**
+     * Returns {@code true} if Pinterest sharing is enabled in page configuration
+     * and the page contains the sharing component, {@code false} otherwise.
+     */
+    public boolean hasPinterestSharing() {
+        return pinterestEnabled && hasSharingComponent;
+    }
+
+    /**
+     * Returns the metadata.
+     */
+    public Map<String, String> getMetadata() {
+        if (metadata == null) {
+            initMetadata();
+        }
+        return metadata;
+    }
+
+    //*************** IMPLEMENTATION *******************
     @Override
     public void activate() throws Exception {
         Page currentPage = getCurrentPage();
         String[] socialMedia = currentPage.getProperties().get("socialMedia", String[].class);
-        if (ArrayUtils.isNotEmpty(socialMedia)) {
-            WebsiteMetada metada = getMetadataProvider(socialMedia);
+        facebookEnabled = ArrayUtils.contains(socialMedia, "facebook");
+        pinterestEnabled = ArrayUtils.contains(socialMedia, "pinterest");
+        socialMediaEnabled = facebookEnabled || pinterestEnabled;
+        if (socialMediaEnabled) {
+            hasSharingComponent = hasSharingComponent(currentPage.getContentResource());
+        }
+    }
+
+    /**
+     * Search a resource tree for sharing component starting from a given resource.
+     *
+     * @param resource the resource
+     *
+     * @return {@code true} if the sharing vomponent was found, {@code false} otherwise
+     */
+    private boolean hasSharingComponent(final Resource resource) {
+        if (resource.isResourceType(SHARING_COMPONENT))
+            return true;
+
+        for (Resource child : resource.getChildren())
+            if (hasSharingComponent(child))
+                return true;
+
+        return false;
+    }
+
+    /**
+     * Prepares Open Graph metadata for a page to be shared on social media services.
+     */
+    private void initMetadata() {
+        metadata = new LinkedHashMap<>();
+        if (socialMediaEnabled) {
+            WebsiteMetada metada = createMetadataProvider();
             put("og:title", metada.getTitle());
             put("og:url", metada.getURL());
             put("og:type", metada.getTypeName());
@@ -68,8 +152,7 @@ public class SocialMediaMetadata extends WCMUsePojo {
             put("og:image", metada.getImage());
             put("og:description", metada.getDescription());
 
-            if (ArrayUtils.contains(socialMedia, "pinterest") &&
-                    metada instanceof ProductMetadata) {
+            if (pinterestEnabled && metada instanceof ProductMetadata) {
                 ProductMetadata productMetadata = (ProductMetadata) metada;
                 put("product:price:amount", productMetadata.getProductPriceAmount());
                 put("product:price:currency", productMetadata.getProductPriceCurrency());
@@ -77,13 +160,39 @@ public class SocialMediaMetadata extends WCMUsePojo {
         }
     }
 
+    /**
+     * Put non-blank named values in metadata map.
+     */
     private void put(String name, String value) {
         if (StringUtils.isNotBlank(value)) {
             metadata.put(name, value);
         }
     }
 
-    private void collectExperienceFragments(final Resource resource, String[] socialMedia, Set<Resource> eFragments) {
+    /**
+     * Instantiates the suitable metadata provider based on the contents of the current page.
+     */
+    private WebsiteMetada createMetadataProvider() {
+        Page currentPage = getCurrentPage();
+        Product product = CommerceHelper.findCurrentProduct(currentPage);
+        Set<Resource> eFragments = new LinkedHashSet<>();
+        collectExperienceFragments(currentPage.getContentResource(), eFragments);
+        if (product == null) {
+            if (eFragments.isEmpty()) {
+                return new WebsiteMetadataProvider();
+            } else {
+                return new ExperienceFragmentWebsiteMetadataProvider(eFragments);
+            }
+        } else {
+            if (eFragments.isEmpty()) {
+                return new ProductMetadataProvider(product);
+            } else {
+                return new ExperienceFragmentProductMetadataProvider(product, eFragments);
+            }
+        }
+    }
+
+    private void collectExperienceFragments(final Resource resource, Set<Resource> eFragments) {
         if (resource.isResourceType(EXPERIENCE_FRAGMENT_CONTAINER)) {
             String fragmentPath = resource.getValueMap().get("fragmentPath", String.class);
             if (StringUtils.isNotBlank(fragmentPath)) {
@@ -94,11 +203,11 @@ public class SocialMediaMetadata extends WCMUsePojo {
                         Resource contentRes = res.getChild("jcr:content");
                         if (contentRes != null) {
                             if (contentRes.isResourceType(FACEBOOK_VARIATION)) {
-                                if (ArrayUtils.contains(socialMedia, "facebook")) {
+                                if (facebookEnabled) {
                                     eFragments.add(contentRes);
                                 }
                             } else if (contentRes.isResourceType(PINTEREST_VARIATION)) {
-                                if (ArrayUtils.contains(socialMedia, "pinterest")) {
+                                if (pinterestEnabled) {
                                     eFragments.add(contentRes);
                                 }
                             }
@@ -108,36 +217,9 @@ public class SocialMediaMetadata extends WCMUsePojo {
             }
         } else {
             for (Iterator<Resource> iter = resource.listChildren(); iter.hasNext();) {
-                collectExperienceFragments(iter.next(), socialMedia, eFragments);
+                collectExperienceFragments(iter.next(), eFragments);
             }
         }
-    }
-
-    private WebsiteMetada getMetadataProvider(String[] socialMedia) {
-        Page currentPage = getCurrentPage();
-        Product product = CommerceHelper.findCurrentProduct(currentPage);
-        Set<Resource> eFragments = new LinkedHashSet<>();
-        collectExperienceFragments(currentPage.getContentResource(), socialMedia, eFragments);
-        if (product == null) {
-            if (eFragments.isEmpty()) {
-                return new WebsiteMetadataProvider();
-            } else {
-                return new ExperienceFragmentWebsiteMetadataProvider(eFragments, socialMedia);
-            }
-        } else {
-            if (eFragments.isEmpty()) {
-                return new ProductMetadataProvider(product);
-            } else {
-                return new ExperienceFragmentProductMetadataProvider(product, eFragments, socialMedia);
-            }
-        }
-    }
-
-    /**
-     * Returns the metadata.
-     */
-    public Map<String, String> getMetadata() {
-        return metadata;
     }
 
     /**
@@ -344,11 +426,9 @@ public class SocialMediaMetadata extends WCMUsePojo {
 
     private class ExperienceFragmentMetadataProvider {
         private Set<Resource> fragmentResources;
-        private String[] socialMedia;
 
-        public ExperienceFragmentMetadataProvider(Set<Resource> fragmentResources, String[] socialMedia) {
+        public ExperienceFragmentMetadataProvider(Set<Resource> fragmentResources) {
             this.fragmentResources = fragmentResources;
-            this.socialMedia = socialMedia;
         }
         //todo extract info from EF
     }
@@ -356,8 +436,8 @@ public class SocialMediaMetadata extends WCMUsePojo {
     private class ExperienceFragmentWebsiteMetadataProvider extends WebsiteMetadataProvider {
         private final ExperienceFragmentMetadataProvider efMetadata;
 
-        public ExperienceFragmentWebsiteMetadataProvider(Set<Resource> eFragments, String[] socialMedia) {
-            efMetadata = new ExperienceFragmentMetadataProvider(eFragments, socialMedia);
+        public ExperienceFragmentWebsiteMetadataProvider(Set<Resource> eFragments) {
+            efMetadata = new ExperienceFragmentMetadataProvider(eFragments);
         }
         //todo provide info from EF
     }
@@ -365,9 +445,9 @@ public class SocialMediaMetadata extends WCMUsePojo {
     private class ExperienceFragmentProductMetadataProvider extends ProductMetadataProvider {
         private final ExperienceFragmentMetadataProvider efMetadata;
 
-        public ExperienceFragmentProductMetadataProvider(Product product, Set<Resource> eFragments, String[] socialMedia) {
+        public ExperienceFragmentProductMetadataProvider(Product product, Set<Resource> eFragments) {
             super(product);
-            efMetadata = new ExperienceFragmentMetadataProvider(eFragments, socialMedia);
+            efMetadata = new ExperienceFragmentMetadataProvider(eFragments);
         }
         //todo provide info from EF
     }
