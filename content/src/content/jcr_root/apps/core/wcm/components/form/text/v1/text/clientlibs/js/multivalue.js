a61b4727759110d97b00858c7e6223c36435fcde
Vani Vrat Srivsatava <vasrivas@adobe.com>
2016-10-24 19:05:50 +0530

    CQ-97275 Adding functionality for custom validation message

--- content/src/content/jcr_root/apps/core/wcm/components/form/text/v1/text/clientlibs/js/multivalue.js
+++ content/src/content/jcr_root/apps/core/wcm/components/form/text/v1/text/clientlibs/js/multivalue.js
@@ -1,4 +1,3 @@
-
 /*******************************************************************************
  * Copyright 2016 Adobe Systems Incorporated
  *
@@ -14,29 +13,27 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  ******************************************************************************/
-(function ($,channel) {
+(function ($, channel) {
     'use strict';
 
     var ADD_ITEM = "[core-form-textField-addItem]";
     var REMOVE_ITEM = "[core-form-textField-item-remove]";
     var TEMPLATE = "[core-form-textField-template]";
-    var CONTENT="[core-form-textField-content]";
-    var CONTAINER ="[core-form-textField-container]";
-    var INPUT_GROUP="[core-form-input-subgroup]";
-    
-    channel.on("click",ADD_ITEM,function(event) {
-		console.log("Add item clicked");
-		var container=$(event.target).parent(CONTAINER)[0];
-		var template = $(container).find(TEMPLATE)[0];
-		var newElement = $(template).clone(true)[0];
-		newElement.removeAttribute("core-form-textField-template");
-		$(container).find(CONTENT)[0].appendChild(newElement);
+    var CONTENT = "[core-form-textField-content]";
+    var CONTAINER = "[core-form-textField-container]";
+    var INPUT_GROUP = "[core-form-input-subgroup]";
+
+    channel.on("click", ADD_ITEM, function (event) {
+        var container = $(event.target).parent(CONTAINER)[0];
+        var template = $(container).find(TEMPLATE)[0];
+        var newElement = $(template).clone(true)[0];
+        newElement.removeAttribute("core-form-textField-template");
+        $(container).find(CONTENT)[0].appendChild(newElement);
     });
 
-    channel.on("click",REMOVE_ITEM,function(event) {
-        console.log("remove item clicked");
+    channel.on("click", REMOVE_ITEM, function (event) {
         var content = $(event.target).parent(INPUT_GROUP)[0];
         content.parentElement.removeChild(content);
     });
 
-})(jQuery,jQuery(document));
+})(jQuery, jQuery(document));
---
+++ content/src/content/jcr_root/apps/core/wcm/components/form/text/v1/text/clientlibs/js/validationMessage.js
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * Copyright 2016 Adobe Systems Incorporated
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+(function ($, channel) {
+    'use strict';
+    var INPUT_FIELD = ".cmp-form-textfield input";
+    var REQUIRED_MSG_ATTRIBUTE = "required-message";
+    var CONSTRAINT_MSG_ATTRIBUTE = "constraint-message";
+
+    channel.ready(function () {
+        $(INPUT_FIELD).each(function (index) {
+            this.oninvalid = function (e) {
+                e.target.setCustomValidity("");
+                if (e.target.validity.typeMismatch) {
+                    if (this.hasAttribute(CONSTRAINT_MSG_ATTRIBUTE)) {
+                        e.target.setCustomValidity(this.getAttribute(CONSTRAINT_MSG_ATTRIBUTE));
+                    }
+                } else if (e.target.validity.valueMissing) {
+                    if (this.hasAttribute(REQUIRED_MSG_ATTRIBUTE)) {
+                        e.target.setCustomValidity(this.getAttribute(REQUIRED_MSG_ATTRIBUTE));
+                    }
+                }
+            }
+            this.oninput = function (e) {
+                e.target.setCustomValidity("");
+            }
+        });
+    });
+
+})(jQuery, jQuery(document));
