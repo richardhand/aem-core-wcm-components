0f992f8f0ac8f898bf178fff659990e65048854f
Burkhard Pauli <pauli@adobe.com>
2016-10-31 15:53:43 +0100

    CQ-97274 - [CoreComponents] New HTL Form Container Component - Phase 1
    revert delete of FormsConstants and FormStructureHelperImpl

---
+++ bundles/core/src/main/java/com/adobe/cq/wcm/core/components/commons/forms/FormsConstants.java
@@ -0,0 +1,31 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ ~ Copyright 2016 Adobe Systems Incorporated
+ ~
+ ~ Licensed under the Apache License, Version 2.0 (the "License");
+ ~ you may not use this file except in compliance with the License.
+ ~ You may obtain a copy of the License at
+ ~
+ ~     http://www.apache.org/licenses/LICENSE-2.0
+ ~
+ ~ Unless required by applicable law or agreed to in writing, software
+ ~ distributed under the License is distributed on an "AS IS" BASIS,
+ ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ~ See the License for the specific language governing permissions and
+ ~ limitations under the License.
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+package com.adobe.cq.wcm.core.components.commons.forms;
+
+/**
+ * Some constants for the form components.
+ */
+public class FormsConstants {
+
+    /** The prefix for all core form related resource types.*/
+    public final static String RT_CORE_FORM_PREFIX = "core/wcm/components/form/";
+
+    /** The resource type for a form container. */
+    public final static String RT_CORE_FORM_CONTAINER = RT_CORE_FORM_PREFIX + "formcontainer";
+
+
+
+}
