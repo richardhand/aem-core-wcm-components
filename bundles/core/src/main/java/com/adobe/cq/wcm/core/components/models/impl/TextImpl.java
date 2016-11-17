31da74ac1697ad548deec90849cf2b360487d6b3
Burkhard Pauli <pauli@adobe.com>
2016-11-17 15:40:33 +0100

    CQ-63187 - [CoreCmp] Improve HTL Text Component - Phase 1
    Fixing Sonar issues

--- bundles/core/src/main/java/com/adobe/cq/wcm/core/components/models/impl/TextImpl.java
+++ bundles/core/src/main/java/com/adobe/cq/wcm/core/components/models/impl/TextImpl.java
@@ -1,18 +1,19 @@
-/*******************************************************************************
- * Copyright 2016 Adobe Systems Incorporated
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- ******************************************************************************/
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+ ~ Copyright 2016 Adobe Systems Incorporated
+ ~
+ ~ Licensed under the Apache License, Version 2.0 (the "License");
+ ~ you may not use this file except in compliance with the License.
+ ~ You may obtain a copy of the License at
+ ~
+ ~     http://www.apache.org/licenses/LICENSE-2.0
+ ~
+ ~ Unless required by applicable law or agreed to in writing, software
+ ~ distributed under the License is distributed on an "AS IS" BASIS,
+ ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ~ See the License for the specific language governing permissions and
+ ~ limitations under the License.
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 package com.adobe.cq.wcm.core.components.models.impl;
 
 import javax.annotation.PostConstruct;
@@ -20,13 +21,16 @@ import javax.inject.Inject;
 
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
-import org.apache.sling.api.resource.ValueMap;
-import org.apache.sling.commons.osgi.PropertiesUtil;
+import org.apache.sling.models.annotations.Default;
 import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
 import com.adobe.cq.wcm.core.components.models.Text;
 
-@Model(adaptables = SlingHttpServletRequest.class, adapters = Text.class, resourceType = TextImpl.RESOURCE_TYPE)
+@Model(adaptables = SlingHttpServletRequest.class,
+       adapters = Text.class,
+       resourceType = TextImpl.RESOURCE_TYPE)
 public class TextImpl implements Text {
 
     public static final String RESOURCE_TYPE = "core/wcm/components/text";
@@ -34,8 +38,13 @@ public class TextImpl implements Text {
     @Inject
     private Resource resource;
 
+    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
     private String text;
-    private boolean isRich;
+
+    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
+    @Default(booleanValues = false)
+    private boolean textIsRich;
+
     private String displayContext;
 
     @Override
@@ -45,7 +54,7 @@ public class TextImpl implements Text {
 
     @Override
     public boolean isRichText() {
-        return isRich;
+        return textIsRich;
     }
 
     @Override
@@ -55,10 +64,7 @@ public class TextImpl implements Text {
 
     @PostConstruct
     private void postConstruct() {
-        ValueMap properties = resource.adaptTo(ValueMap.class);
-        text = PropertiesUtil.toString(properties.get("text"), null);
-        isRich = PropertiesUtil.toBoolean(properties.get("textIsRich"), false);
-        if (isRich) {
+        if (textIsRich) {
             displayContext = CONTEXT_HTML;
         } else {
             displayContext = CONTEXT_TEXT;
