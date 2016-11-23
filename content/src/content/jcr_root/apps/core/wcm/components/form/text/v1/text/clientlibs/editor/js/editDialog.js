b8b216a9342faf2f7ad30bf2db2f320e7fda22ed
Vani Vrat Srivsatava <vasrivas@adobe.com>
2016-11-24 03:16:34 +0530

    CQ-106470 Conditionally displaying the various edit dialog fields * Constraint message field based on input type * Required message based on required field * Required and Readonly should be mutually exclusive

--- content/src/content/jcr_root/apps/core/wcm/components/form/text/v1/text/clientlibs/editor/js/editDialog.js
+++ content/src/content/jcr_root/apps/core/wcm/components/form/text/v1/text/clientlibs/editor/js/editDialog.js
@@ -15,8 +15,16 @@
  ******************************************************************************/
 (function ($, channel, Coral) {
     'use strict';
+
+    var EDIT_DIALOG = ".cmp-form-textfield-editDialog";
     var TEXTFIELD_TYPES = ".cmp-form-textfield-types";
     var TEXTFIELD_ROWS = ".cmp-form-textfield-rows";
+    var TEXTFIELD_REQUIRED = ".cmp-form-textfield-required";
+    var TEXTFIELD_CONSTRAINTMESSAGE = ".cmp-form-textfield-constraintmessage";
+    var TEXTFIELD_REQUIREDMESSAGE = ".cmp-form-textfield-requiredmessage";
+    var TEXTFIELD_READONLY = ".cmp-form-textfield-readonly";
+    var TEXTFIELD_READONLYSELECTED_ALERT = ".cmp-form-textfield-readonlyselected-alert";
+    var TEXTFIELD_REQUIREDSELECTED_ALERT = ".cmp-form-textfield-requiredselected-alert";
 
     function checkAndDisplay(element, expectedValue, actualValue) {
         if (expectedValue === actualValue) {
@@ -26,24 +34,78 @@
         }
     }
 
+    function handleTextarea(dialog) {
+        var component = dialog.find(TEXTFIELD_TYPES)[0];
+        var textfieldRows = dialog.find(TEXTFIELD_ROWS);
+        checkAndDisplay(textfieldRows,
+            "textarea",
+            component.value);
+        component.on("change", function () {
+            checkAndDisplay(textfieldRows,
+                "textarea",
+                component.value);
+        });
+    }
+
+    function handleConstraintMessage(dialog) {
+        var component = dialog.find(TEXTFIELD_TYPES)[0];
+        var constraintMessage = dialog.find(TEXTFIELD_CONSTRAINTMESSAGE);
+        var displayConstraintMessage = component.value !== "text" && component.value !== "textarea";
+        checkAndDisplay(constraintMessage,
+            true,
+            displayConstraintMessage);
+        component.on("change", function () {
+            displayConstraintMessage = this.value !== "text" && this.value !== "textarea";
+            checkAndDisplay(constraintMessage,
+                true,
+                displayConstraintMessage);
+        });
+    }
+
+    function handleRequiredMessage(dialog) {
+        var component = dialog.find(TEXTFIELD_REQUIRED)[0];
+        var requiredMessage = dialog.find(TEXTFIELD_REQUIREDMESSAGE);
+        checkAndDisplay(requiredMessage,
+            true,
+            component.checked);
+        component.on("change", function () {
+            checkAndDisplay(requiredMessage,
+                true,
+                component.checked);
+        });
+    }
+
+    function handleExclusion(component1, component2, alert) {
+        component1.on("change", function () {
+            if (this.checked && component2.checked) {
+                alert.show();
+                component2.set("checked", false, true);
+            }
+        });
+    }
+
     function initialise(dialog) {
         dialog = $(dialog);
-        if (dialog.find(TEXTFIELD_TYPES).length > 0) {
-            Coral.commons.ready(dialog.find(TEXTFIELD_TYPES)[0], function (component) {
-                checkAndDisplay(dialog.find(TEXTFIELD_ROWS),
-                    "textarea",
-                    component.value);
-                component.on("change", function (e) {
-                    checkAndDisplay(dialog.find(TEXTFIELD_ROWS),
-                        "textarea",
-                        this.value);
-                });
-            });
-        }
+        handleTextarea(dialog);
+        handleConstraintMessage(dialog);
+        handleRequiredMessage(dialog);
+
+        var readonly = dialog.find(TEXTFIELD_READONLY)[0];
+        var required = dialog.find(TEXTFIELD_REQUIRED)[0];
+        handleExclusion(readonly,
+            required,
+            dialog.find(TEXTFIELD_REQUIREDSELECTED_ALERT)[0]);
+        handleExclusion(required,
+            readonly,
+            dialog.find(TEXTFIELD_READONLYSELECTED_ALERT)[0]);
     }
 
     channel.on("foundation-contentloaded", function (e) {
-        initialise(e.target);
+        if ($(e.target).find(EDIT_DIALOG).length > 0) {
+            Coral.commons.ready(e.target, function (component) {
+                initialise(component);
+            });
+        }
     });
 
 })(jQuery, jQuery(document), Coral);
