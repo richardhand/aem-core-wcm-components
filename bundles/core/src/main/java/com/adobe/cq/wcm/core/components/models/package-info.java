6ef1292d6add3f4ee88aa0936e1bbdd10720487e
Burkhard Pauli <pauli@adobe.com>
2016-11-24 09:25:16 +0100

    CQ-63184 - [CoreCmp] Improve HTL List Component - Phase 1
    - increased model version

--- bundles/core/src/main/java/com/adobe/cq/wcm/core/components/models/package-info.java
+++ bundles/core/src/main/java/com/adobe/cq/wcm/core/components/models/package-info.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  ******************************************************************************/
 
-@Version("0.11.0")
+@Version("0.12.0")
 package com.adobe.cq.wcm.core.components.models;
 
 import org.osgi.annotation.versioning.Version;
