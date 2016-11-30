3a63006a9bb48973267dd9f3f868ccc163576e72
Christophe Jelger <jelger@adobe.com>
2016-11-30 11:20:08 +0100

    CQ-108234 - Renaming the jcr node core/wcm/components/form/formcontainer breaks many form features
    Fixed the remaining issues due to component versioning.

--- bundles/core/src/main/java/com/adobe/cq/wcm/core/components/commons/forms/package-info.java
+++ bundles/core/src/main/java/com/adobe/cq/wcm/core/components/commons/forms/package-info.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  ******************************************************************************/
-@Version("0.10.0")
+@Version("0.11.0")
 package com.adobe.cq.wcm.core.components.commons.forms;
 
 import org.osgi.annotation.versioning.Version;
