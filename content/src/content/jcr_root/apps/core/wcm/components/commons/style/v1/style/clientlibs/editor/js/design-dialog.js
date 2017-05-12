611cb7265acf4e56bfe0d59157689b1da72a294c
jkautzma <jkautzma@adobe.com>
2017-05-12 11:19:38 +0200

    CQ-4206818 - [CoreCmp] Improve the Style tab component
    - remove the default style widget and its js logic
    - add a checkbox “Allow no style”
    - add a hidden field to set the style ID with a timestamp when the dialog is POST-ed

--- content/src/content/jcr_root/apps/core/wcm/components/commons/style/v1/style/clientlibs/editor/js/design-dialog.js
+++ content/src/content/jcr_root/apps/core/wcm/components/commons/style/v1/style/clientlibs/editor/js/design-dialog.js
@@ -16,94 +16,22 @@
 
 /**
  * Design dialog of the Style Tab:
- * - The options of the select field to define the default value are dynamically added/removed/updated
- *   when the multifield options are edited
+ * - Set the style ID hidden parameter with a timestamp when a new style is added
  */
 (function ($, Granite, ns, $document) {
 
-    var DEFAULT_STYLE_SELECTOR = "coral-select.core-styletab-style-default",
-        MULTIFIELD_STYLES_SELECTOR = "coral-multifield.core-styletab-styles",
-        STYLE_NAME_SELECTOR = "coral-multifield .core-styletab-style-name";
+    var MULTIFIELD_STYLES_SELECTOR = "coral-multifield.core-styletab-styles",
+        STYLE_ID_SELECTOR = ".core-styletab-style-ID";
 
-    // Hide the dropdown to select the default style if the allowed styles defined in the multifield are empty,
-    // show it otherwise
-    function hideShowSelect(select, multifield) {
-
-        if (select === null || select === undefined || multifield === null || multifield === undefined) {
-            return;
-        }
-
-        if (multifield.items.length == 0) {
-            $(select).parent().hide();
-        } else {
-            $(select).parent().show();
-        }
-    }
-
-    // Update the select field that defines the default value
-    function updateDefaultStyleSelect() {
-
-        var select = $(DEFAULT_STYLE_SELECTOR).get(0),
-            multifield = $(MULTIFIELD_STYLES_SELECTOR).get(0);
-
-        if (select === null || select === undefined || multifield === null || multifield === undefined) {
-            return;
-        }
-
-        // clear the select items to work around a Coral.Select issue (CUI-5584)
-        select.items.clear();
-
-        // add the default option
-        var noneItem = new Coral.Select.Item();
-        noneItem.content.textContent = "None";
-        noneItem.value = "";
-        select.items.add(noneItem);
-
-        // for each multifield style name, add an option to the default styles dropdown
-        $(STYLE_NAME_SELECTOR).each(function (i, style) {
-            var newItem = new Coral.Select.Item();
-            newItem.content.textContent = $(style).val();
-            newItem.value = $(style).val();
-            select.items.add(newItem);
-        });
-
-        // Note: we use Coral.commons.nextFrame to make sure that the select widget has been updated
-        Coral.commons.nextFrame(function() {
-            hideShowSelect(select, multifield);
-        });
-    }
-
-    // Update the default size select when a style is added, removed or updated
+    // Set the style ID hidden parameter when a new style is added
     $document.on("change", MULTIFIELD_STYLES_SELECTOR, function(e) {
-        updateDefaultStyleSelect();
-    });
-
-    // When loading the dialog, hide/show the dropdown to select the default style
-    $document.on("foundation-contentloaded", function (e) {
-        Coral.commons.ready($(DEFAULT_STYLE_SELECTOR), function(component) {
-            var select = component.get(0),
-                multifield = $(MULTIFIELD_STYLES_SELECTOR).get(0);
-            hideShowSelect(select, multifield);
+        // if the style ID value is not defined, we set it with a timestamp
+        $(STYLE_ID_SELECTOR).each(function(index, element) {
+            var value = $(element).val();
+            if (!value) {
+                $(element).val(Date.now());
+            }
         });
-    });
-
-    // temporary workaround until CQ-4206495 and CUI-1818 are fixed:
-    // add a margin when opening the dropdown
-    $document.on("coral-select:showitems", DEFAULT_STYLE_SELECTOR, function(e) {
-        var select = e.currentTarget,
-            buttonHeight = $(select).find("button").outerHeight(true),
-            count = select.items.length,
-            totalHeight = count * (buttonHeight + 5),
-            maxHeight = parseInt($(select).find("coral-selectlist").css("max-height"),10),
-            marginBottom = Math.min(totalHeight, maxHeight);
-        $(select).css('margin-bottom', marginBottom);
-    });
-
-    // temporary workaround until CQ-4206495 and CUI-1818 are fixed:
-    // remove the margin when closing the dropdown
-    $document.on("coral-select:hideitems", DEFAULT_STYLE_SELECTOR, function(e) {
-        var select = e.currentTarget;
-        $(select).css('margin-bottom', 0);
     });
 
 }(jQuery, Granite, Granite.author, jQuery(document)));
