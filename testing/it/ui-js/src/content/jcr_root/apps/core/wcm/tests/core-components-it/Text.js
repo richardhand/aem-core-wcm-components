a7e7dd8d3fc2107f679d8369b53feacaf72b19b3
Vlad Băilescu <bailescu@adobe.com>
2017-06-30 16:49:41 +0300

    CQ-4211658 - [CoreCmp] Refactor sandbox code so more can be shared between v1 and sandbox
    * Refactored Text tests to be reusable
    
    (RTC not needed)

--- testing/it/ui-js/src/content/jcr_root/apps/core/wcm/tests/core-components-it/Text.js
+++ testing/it/ui-js/src/content/jcr_root/apps/core/wcm/tests/core-components-it/Text.js
@@ -14,93 +14,108 @@
  * limitations under the License.
  ******************************************************************************/
 
+window.CQ.CoreComponentsIT.v1.Text = window.CQ.CoreComponentsIT.v1.Text || {}
+
 /**
  * Tests for the core text component
  */
 ;(function (h, $) {
 
-    // shortcut
+    // shortcuts
     var c = window.CQ.CoreComponentsIT.commons;
+    var text = window.CQ.CoreComponentsIT.v1.Text;
 
     var testValue = '<b>This</b> is a <i>rich</i> <u>text</u>.';
 
     /**
      * Before Test Case
      */
-    var tcExecuteBeforeTest = new TestCase("Setup Before Test")
-        // common set up
-        .execTestCase(c.tcExecuteBeforeTest)
-        // create the test page, store page path in 'testPagePath'
-        .execFct(function (opts,done) {
-            c.createPage(c.template, c.rootPage ,'page_' + Date.now(),"testPagePath",done)
-        })
-        // add the component, store component path in 'cmpPath'
-        .execFct(function (opts, done){
-            c.addComponent(c.rtText, h.param("testPagePath")(opts)+c.relParentCompPath,"cmpPath",done)
-        })
-        // open the new page in the editor
-        .navigateTo("/editor.html%testPagePath%.html");
+    text.tcExecuteBeforeTest = function (textRT, pageRT) {
+        return new TestCase("Setup Before Test")
+            // common set up
+            .execTestCase(c.tcExecuteBeforeTest)
+            // create the test page, store page path in 'testPagePath'
+            .execFct(function (opts,done) {
+                c.createPage(c.template, c.rootPage, 'page_' + Date.now(), "testPagePath", done, pageRT)
+            })
+            // add the component, store component path in 'cmpPath'
+            .execFct(function (opts, done){
+                c.addComponent(textRT, h.param("testPagePath")(opts) + c.relParentCompPath, "cmpPath", done)
+            })
+            // open the new page in the editor
+            .navigateTo("/editor.html%testPagePath%.html");
+    };
 
     /**
      * After Test Case
      */
-    var tcExecuteAfterTest = new TestCase("Clean up after Test")
-        // common clean up
-        .execTestCase(c.tcExecuteAfterTest)
-        // delete the test page we created
-        .execFct(function (opts, done) {
-            c.deletePage(h.param("testPagePath")(opts), done);
-        });
+    text.tcExecuteAfterTest = function() {
+        return new TestCase("Clean up after Test")
+            // common clean up
+            .execTestCase(c.tcExecuteAfterTest)
+            // delete the test page we created
+            .execFct(function (opts, done) {
+                c.deletePage(h.param("testPagePath")(opts), done);
+            });
+    };
 
     /**
      * Test: Check if text is stored/rendered correctly using the inline editor
      */
-    var setTextValueUsingInlineEditor = new h.TestCase('Set text using inline editor',{
-        execBefore: tcExecuteBeforeTest,
-        execAfter: tcExecuteAfterTest})
+    text.tcSetTextValueUsingInlineEditor = function(tcExecuteBeforeTest, tcExecuteAfterTest) {
+        return new h.TestCase('Set text using inline editor',{
+            execBefore: tcExecuteBeforeTest,
+            execAfter: tcExecuteAfterTest})
 
-        // open the inline editor
-        .execTestCase(c.tcOpenInlineEditor("cmpPath"))
+            // open the inline editor
+            .execTestCase(c.tcOpenInlineEditor("cmpPath"))
 
-        //switch to the content frame
-        .config.changeContext(c.getContentFrame)
+            //switch to the content frame
+            .config.changeContext(c.getContentFrame)
 
-        // set the example text
-        .execFct(function() {
-            h.find('.cmp-text.aem-GridColumn p').html(testValue);
-        })
+            // set the example text
+            .execFct(function() {
+                h.find('.cmp-text.aem-GridColumn p').html(testValue);
+            })
 
-        // switch back to edit frame
-        .config.resetContext()
+            // switch back to edit frame
+            .config.resetContext()
 
-        // click on save on the inline editor toolbar
-        .execTestCase(c.tcSaveInlineEditor)
+            // click on save on the inline editor toolbar
+            .execTestCase(c.tcSaveInlineEditor)
 
-        //switch to the content frame
-        .config.changeContext(c.getContentFrame)
+            //switch to the content frame
+            .config.changeContext(c.getContentFrame)
 
-        // check if the text is rendered
-        .assert.isTrue(
-        function() {
-            var actualValue = h.find('.cmp-text.aem-GridColumn  p').html();
-            return actualValue === testValue;
-        })
+            // check if the text is rendered
+            .assert.isTrue(
+            function() {
+                var actualValue = h.find('.cmp-text.aem-GridColumn  p').html();
+                return actualValue === testValue;
+            })
 
-        // swith back to edit frame
-        .config.resetContext()
+            // swith back to edit frame
+            .config.resetContext()
 
-        // reload the page, to see if the text really got saved
-        .navigateTo("/editor.html%testPagePath%.html")
+            // reload the page, to see if the text really got saved
+            .navigateTo("/editor.html%testPagePath%.html")
 
-        //switch to the content frame
-        .config.changeContext(c.getContentFrame)
+            //switch to the content frame
+            .config.changeContext(c.getContentFrame)
 
-        // check again if the text is still there
-        .assert.isTrue(
-        function() {
-            var actualValue = h.find('.cmp-text.aem-GridColumn  p').html();
-            return actualValue === testValue;
-        });
+            // check again if the text is still there
+            .assert.isTrue(
+            function() {
+                var actualValue = h.find('.cmp-text.aem-GridColumn  p').html();
+                return actualValue === testValue;
+            });
+    };
+
+    /**
+     * v1 specifics
+     */
+    var tcExecuteBeforeTest = text.tcExecuteBeforeTest(c.rtText)
+    var tcExecuteAfterTest = text.tcExecuteAfterTest();
 
     /**
      * The main test suite for Text Component
@@ -109,6 +124,5 @@
         execBefore:c.tcExecuteBeforeTestSuite,
         execInNewWindow : false})
 
-        .addTestCase(setTextValueUsingInlineEditor)
-    ;
+        .addTestCase(text.tcSetTextValueUsingInlineEditor(tcExecuteBeforeTest, tcExecuteAfterTest));
 }(hobs, jQuery));
