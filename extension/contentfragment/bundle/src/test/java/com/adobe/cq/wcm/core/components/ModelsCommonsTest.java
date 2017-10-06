bd8e375059437ea5f638e3bfe4958d5ea882c51f
Burkhard Pauli <pauli@adobe.com>
2017-10-06 12:21:00 +0200

    CQ-4221724 - [CoreCmp] Extension Model for core-component project (#151)
    * CQ-4221724 - [CoreCmp] Extension Model for core-component project
    
    * Added dedicated content package and bundle for content fragment extension
    * Moved contentfragment component to new content-package and bundle
    * Bumped versions because of moved classes
    * Moved extension artifacts to dedicated directory
    * Created wrapper package for extensions
    * Updated reactor + all build profile
    * Added rat plugin exclusions
    * Cleaned up dependencies for new extension modules
    * Extracted common tests for model default methods to testing module
    * Consistent naming (extension vs extensions)
    * Updated content-package maven plugin (was not reinstalling snapshot packages correctly)
    * Updated Jenkinsfile with dependencies to fix branch build errors
    * Fixed filter.xml typo
    * Updated versions after merging changes from PRIVATE_master
    * Added extensions to Jenkinsfile
    * Added FP dependency
    
    (RTC not needed)

--- extension/contentfragment/bundle/src/test/java/com/adobe/cq/wcm/core/components/ModelsCommonsTest.java
+++ extension/contentfragment/bundle/src/test/java/com/adobe/cq/wcm/core/components/ModelsCommonsTest.java
@@ -13,7 +13,19 @@
  ~ See the License for the specific language governing permissions and
  ~ limitations under the License.
  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-@Version("3.0.0")
-package com.adobe.cq.wcm.core.components.sandbox.models;
+package com.adobe.cq.wcm.core.components;
 
-import org.osgi.annotation.versioning.Version;
+import org.junit.Test;
+
+import com.adobe.cq.wcm.core.components.testing.AbstractModelTest;
+
+public class ModelsCommonsTest extends AbstractModelTest{
+
+    @Test
+    public void testDefaultBehaviour() throws Exception {
+        testDefaultBehaviour(new String[] {
+                "com.adobe.cq.wcm.core.components.sandbox.extension.contentfragment.models"
+        });
+    }
+
+}
